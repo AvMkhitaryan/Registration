a04d93ddebb5b0913aa385799016d6f461a52e22
Avetik Mkhitaryan <av_mkhitaryan@bk.ru>
2019-12-25 17:56:13 +0400

    95% project only good

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,12 @@
+$(document).on('click', '.reset', function () {
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("country").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("datepicker").value = "";
+    document.getElementsByClassName("form-check-input-style").value = "";
+});
+
 $.ajax({
     url: "json/index.json",
     data: "infoRegistObject",
@@ -7,7 +16,9 @@ $.ajax({
 
         for (let i in data.infoRegistObject) {
 
+
             let tr = document.createElement("tr");
+            // tr.setAttribute("id",i);
             tBody.appendChild(tr);
 
             let tdOne = document.createElement("td");
@@ -44,6 +55,9 @@ $.ajax({
 
             let tdSevenButtonOne = document.createElement("button");
             tdSevenButtonOne.setAttribute("class", "btn btn-success btn-edit");
+            tdSevenButtonOne.setAttribute("data-toggle", "tab");
+            tdSevenButtonOne.setAttribute("href", "#home");
+            tdSevenButtonOne.setAttribute("onclick", "edit(" + (+i) + ")");
             tdSevenDiv.appendChild(tdSevenButtonOne);
 
             let editIcon = document.createElement("i");
@@ -52,19 +66,28 @@ $.ajax({
             tdSevenButtonOne.appendChild(editIcon);
 
             let tdSevenButtonTwo = document.createElement("button");
-            tdSevenButtonTwo.setAttribute("class", "btn btn-danger btn-delete");
+            tdSevenButtonTwo.setAttribute("class", "btn btn-danger btn-delete row-remove");
             tdSevenDiv.appendChild(tdSevenButtonTwo);
 
             let deleteIcon = document.createElement("i");
             deleteIcon.setAttribute("class", "fa fa-trash");
-            deleteIcon.setAttribute("aria-hidden", "true");
+            // deleteIcon.setAttribute("onclick","remove("+i+")");
             tdSevenButtonTwo.appendChild(deleteIcon);
 
         }
+        $('.row-remove').click(function () {
+            var result = confirm("Want to delete?");
+            if (result === true) {
+                $(this).closest('tr').remove();
+            } else {
+                alert("right choice")
+            }
+        });
     }, error: function (error) {
         console.log(error)
     }
 });
+
 
 let dbInfoObj = [
     {
@@ -97,65 +120,121 @@ let dbInfoObj = [
 
 ];
 
-$(document).on('click', '#registButtonId', function () {
-    let newObj = {
-        "name": document.getElementById("firstName").value,
-        "surname": document.getElementById("lastName").value,
-        "country": document.getElementById("country").value,
-        "city": document.getElementById("city").value,
-        "dateOfBirth": document.getElementById("datepicker").value,
-        "gender": document.getElementsByClassName("form-check-input-style").value,
+var arr=[];
 
-    };
+function edit(arg) {
+    arr.push(dbInfoObj[arg].id);
 
-    dbInfoObj.push(newObj);
-    console.log(dbInfoObj);
+    document.getElementById("firstName").value = dbInfoObj[arg].name;
+    document.getElementById("lastName").value = dbInfoObj[arg].surname;
+    document.getElementById("country").value = dbInfoObj[arg].country;
+    document.getElementById("city").value = dbInfoObj[arg].city;
+    document.getElementById("datepicker").value = dbInfoObj[arg].dateOfBirth;
+    document.getElementsByClassName("form-check-input-style").value = dbInfoObj[arg].gender;
 
-    let tbody = document.getElementById("tables-tbody");
+    $(".change-div").css("display", "block");
+    // console.log(argumnets);
+    // console.log(arr);
+    // console.log(dbInfoObj);
+}
 
-    for (let i = 0; i < dbInfoObj.length; i++) {
-        var obj = dbInfoObj[i];
+
+function changeInfor(){
+
+    dbInfoObj.arr[0].name =document.getElementById("firstName").value;
+    dbInfoObj.arr[0].surname = document.getElementById("lastName").value;
+    dbInfoObj.arr[0].country =document.getElementById("country").value;
+    dbInfoObj.arr[0].city =document.getElementById("city").value ;
+    dbInfoObj.arr[0].dateOfBirth =document.getElementById("datepicker").value ;
+    dbInfoObj.arr[0].gender =document.getElementsByClassName("form-check-input-style").value ;
+    console.log(arr[0]);
+    $(".change-div").css("display", "none");
+}
+
+
+$(document).on('click', '.Registration', function () {
+
+    $(".form-control").each(function () {
+
+        if ($(this).val().trim() == '') {
+            $(this).css('border-color', 'red');
+        } else {
+            $(this).css('border-color', '');
+        }
+    });
+
+    if (document.getElementById("firstName").value === "" ||
+        document.getElementById("lastName").value === "" ||
+        document.getElementById("country").value === "" ||
+        document.getElementById("city").value === "" ||
+        document.getElementById("datepicker").value === "") {
+
+    } else {
+        let newObj = {
+            "name": document.getElementById("firstName").value,
+            "surname": document.getElementById("lastName").value,
+            "country": document.getElementById("country").value,
+            "city": document.getElementById("city").value,
+            "dateOfBirth": document.getElementById("datepicker").value,
+            "gender": document.getElementsByClassName("form-check-input-style").value,
+
+        };
+
+        dbInfoObj.push(newObj);
+
+        let tbody = document.getElementById("tables-tbody");
+
+        for (let i = 0; i < dbInfoObj.length; i++) {
+            var obj = dbInfoObj[i];
+        }
+
+        let th = tbody.insertRow(3);
+
+        let tdOne = th.insertCell(0);
+        tdOne.innerHTML = obj.name;
+
+        let tdTwo = th.insertCell(1);
+        tdTwo.innerHTML = obj.surname;
+
+        let tdThree = th.insertCell(2);
+        tdThree.innerHTML = obj.country;
+
+        let tdFore = th.insertCell(3);
+        tdFore.innerHTML = obj.city;
+
+        let tdFive = th.insertCell(4);
+        tdFive.innerHTML = obj.dateOfBirth;
+
+        let tdSix = th.insertCell(5);
+        tdSix.innerHTML = obj.gender;
+
+        let tdSev = th.insertCell(6);
+        tdSev.innerHTML = `
+            <div class="d-flex">
+                   <button class="btn btn-success"><i class="fa fa-pencil" aria-hidden="true"></i> </button>
+                   <button class="btn btn-danger btn-delete row-remove"><i class="fa fa-trash" aria-hidden="true"></i></button>
+            </div>
+    `;
+        $('.row-remove').click(function () {
+            var result = confirm("Want to delete?");
+            if (result === true) {
+                $(this).closest('tr').remove();
+            } else {
+                alert("right choice")
+            }
+        });
     }
 
-    console.log(dbInfoObj.length);
-    let th = tbody.insertRow(3);
-
-    let tdOne = th.insertCell(0);
-    tdOne.innerHTML = obj.name;
-
-    let tdTwo = th.insertCell(1);
-    tdTwo.innerHTML = obj.surname;
-
-    let tdThree = th.insertCell(2);
-    tdThree.innerHTML = obj.country;
-
-    let tdFore = th.insertCell(3);
-    tdFore.innerHTML = obj.city;
-
-    let tdFive = th.insertCell(4);
-    tdFive.innerHTML = obj.dateOfBirth;
-
-    let tdSix = th.insertCell(5);
-    tdSix.innerHTML = obj.gender;
-
-    let tdSev = th.insertCell(6);
-    tdSev.innerHTML = `
-            <div class="d-flex">
-                               <button class="btn btn-success"><i class="fa fa-pencil" aria-hidden="true"></i> </button>
-                                  <button class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></button>
-                               </div>
-    `
 });
 
-$(document).on('click', '.btn-edit', function () {
-
-    let name = $("#firstName").val();
-    let surname = $("#lastName").val();
-    let country = $("#country").val();
-    let city = $("#city").val();
-    let dateOfBirth = $("#datepicker").val();
-    let gender = $(".form-check-input-style").val();
+$(".btn-edit").click(function(){
+    // $("#nav-item-first-a").removeClass("nav-item");
+    $("#nav-item-first-a").addClass("nav-item active");
 });
+
+
+
+
 
 
 
